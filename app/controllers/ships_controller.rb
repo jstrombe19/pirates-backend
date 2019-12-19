84189c47a778f338982b4b9d4d93a0d73665d26b
jstrombe19 <jaredstromberg@gmail.com>
2019-12-19 10:44:35 -0700

    Deleted most of ships controller.

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -12,42 +12,4 @@ class ShipsController < ApplicationController
   def show
   end
 
-  # POST /ships
-  # POST /ships.json
-  def create
-    @ship = Ship.new(ship_params)
-
-    if @ship.save
-      render :show, status: :created, location: @ship
-    else
-      render json: @ship.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /ships/1
-  # PATCH/PUT /ships/1.json
-  def update
-    if @ship.update(ship_params)
-      render :show, status: :ok, location: @ship
-    else
-      render json: @ship.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /ships/1
-  # DELETE /ships/1.json
-  def destroy
-    @ship.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_ship
-      @ship = Ship.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def ship_params
-      params.require(:ship).permit(:name, :captain, :cursed)
-    end
-end
+  
